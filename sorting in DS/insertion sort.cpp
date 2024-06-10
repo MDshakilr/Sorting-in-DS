9fe66525957f49d1726bb72edfc44cc748e4538d
Md. Shakil Rana <159621858+MDshakilr@users.noreply.github.com>
2024-06-10 18:47:17 +0600

    Update insertion sort.cpp

--- sorting in DS/insertion sort.cpp
+++ sorting in DS/insertion sort.cpp
@@ -1,3 +1,17 @@
+/*Insertion-Sort(A)
+for i = 1 to A.length
+   key = A[i]
+   j = i– 1
+   while j >= 0 and A[j] > key
+      A[j + 1] = A[j]
+      j= j -1
+   A[j+ 1] = key
+
+    */
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
